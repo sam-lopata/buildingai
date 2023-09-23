de44d7dd9d9e5d89bb2090265533a5babf790ede
Sam <sam.lopata@gmail.com>
2023-09-23 15:58:33 +0300

    p5a

---
+++ p5a.py
@@ -0,0 +1,27 @@
+import random
+import numpy as np
+
+
+def accept_prob(S_old, S_new, T):
+    # this is the acceptance "probability" in the greedy hill-climbing method
+    # where new solutions are accepted if and only if they are better
+    # than the old one.
+    # change it to be the acceptance probability in simulated annealing
+
+    if S_new > S_old:
+        return 1.0
+    else:
+        return np.exp(-(S_old-S_new)/T)
+    
+
+# the above function will be used as follows. this is shown just for
+# your information; you don't have to change anything here
+def accept(S_old, S_new, T):
+    prob = accept_prob(S_old, S_new, T)
+    print(prob)
+    if random.random() < prob:
+        print(True)
+    else:
+        print(False)
+        
+accept(150, 140, 10)        
